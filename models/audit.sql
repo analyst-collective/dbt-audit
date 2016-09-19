441c650d46b07c7eb9c36907e5fedd4670231124
Drew Banin <drew@8tracks.com>
2016-09-19 17:30:04 -0400

    add compiled_at field

--- models/audit.sql
+++ models/audit.sql
@@ -1,6 +1,7 @@
 {{ config(materialized="incremental", sql_where="FALSE") }}
 
 select
+'2016-01-01 00:00:00'::timestamp as "compiled_at",
 'model_name'::varchar as "model",
 '2016-01-01 00:00:00'::timestamp as "timestamp",
 'status'::varchar as status
