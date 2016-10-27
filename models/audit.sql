048a02105116d6dd5d2060906d5eaa1fd958e39e
Drew Banin <drew@8tracks.com>
2016-10-26 23:37:45 -0400

    compiled_at --> run_started_at

--- models/audit.sql
+++ models/audit.sql
@@ -1,7 +1,7 @@
 {{ config(materialized="incremental", sql_where="FALSE") }}
 
 select
-'2016-01-01 00:00:00'::timestamp as "compiled_at",
+'2016-01-01 00:00:00'::timestamp as "run_started_at",
 'model_name'::varchar as "model",
 '2016-01-01 00:00:00'::timestamp as "timestamp",
 'status'::varchar as status
