bd99d6da92ecdb85c64db0c64cb6aa020838c223
Drew Banin <drew@8tracks.com>
2016-10-26 23:38:42 -0400

    add invocation_id

--- models/audit.sql
+++ models/audit.sql
@@ -3,6 +3,7 @@
 select
 '2016-01-01 00:00:00'::timestamp as "run_started_at",
 'model_name'::varchar as "model",
+'id'::varchar(32) as "invocation_id",
 '2016-01-01 00:00:00'::timestamp as "timestamp",
 'status'::varchar as status
 limit 0
